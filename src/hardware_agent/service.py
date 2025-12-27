c5b489b67a3032c6c8a85a1376730cbcfd1de6f8
Alistair White-Horne <dapeefyone@gmail.com>
2025-12-27 22:05:48 +0000

    Restructure the whole app
    This improves the import structure, allowing `chicken_watcher` to
    be installed by `hardware_agent` more easily.

---
+++ src/hardware_agent/service.py
@@ -0,0 +1,21 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
+django.setup()
+
+# Must be below `django.setup()` for the imports to work correctly
+from chicken_watcher.models import Egg, Chicken, NestingBox  # noqa: E402
+
+
+def main():
+    nesting_box = NestingBox.objects.all().first()
+    chicken = Chicken.objects.all().first()
+
+    for i in range(10):
+        print(f"Creating egg number {i + 1}")
+        Egg.objects.create(chicken=chicken, nesting_box=nesting_box)
+
+
+if __name__ == "__main__":
+    main()
