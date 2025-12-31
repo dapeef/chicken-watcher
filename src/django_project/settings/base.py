9042c299b5677852a4491a7a092ebeeb173c3556
Alistair White-Horne <dapeefyone@gmail.com>
2025-12-31 18:36:06 +0000

    Make django secret an env variable

--- src/django_project/settings/base.py
+++ src/django_project/settings/base.py
@@ -13,15 +13,19 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+load_dotenv(BASE_DIR.parent / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-u5d9jz%oz*c%m58xdf!6p3!=)^1iw2obgwnsik*xdbtv9@-rl_"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 
 ALLOWED_HOSTS = [
