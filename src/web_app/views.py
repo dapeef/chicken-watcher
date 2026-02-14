88063a0816925b64053ca7e098752becbc750cfc
Alistair White-Horne <dapeefyone@gmail.com>
2026-02-14 12:46:58 +0000

    Make the dashboard refresh of its own accord

--- src/web_app/views.py
+++ src/web_app/views.py
@@ -5,6 +5,7 @@ from django import db
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from django.shortcuts import render
 
 from .forms import EggForm
 from .models import Egg, Chicken, NestingBoxPresence, HardwareSensor, NestingBoxImage
@@ -13,42 +14,73 @@ from django.db.models import Count, Max, Q
 from .utils import rolling_average, RIGHT
 
 
+def get_dashboard_context():
+    today_start = timezone.localtime().replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
+    now = timezone.localtime()
+
+    eggs_today_qs = Egg.objects.filter(laid_at__range=(today_start, now))
+    
+    # Latest presence for each box
+    if db.connection.vendor == "postgresql":
+        latest_presence = (
+            NestingBoxPresence.objects.filter(present_at__gte=today_start)
+            .order_by("nesting_box_id", "-present_at")
+            .distinct("nesting_box_id")
+        )
+    else:
+        # Fallback for SQLite: get the latest presence for each nesting box manually or just show recent ones
+        latest_presence = []
+        box_ids = NestingBoxPresence.objects.filter(present_at__gte=today_start).values_list('nesting_box_id', flat=True).distinct()
+        for b_id in box_ids:
+            p = NestingBoxPresence.objects.filter(nesting_box_id=b_id, present_at__gte=today_start).order_by('-present_at').first()
+            if p:
+                latest_presence.append(p)
+
+    return {
+        "eggs_today": eggs_today_qs.count(),
+        "laid_chickens": Chicken.objects.filter(egg__in=eggs_today_qs).distinct().order_by("name"),
+        "latest_presence": latest_presence,
+        "latest_events": Egg.objects.filter(laid_at__gte=today_start)
+            .select_related("chicken", "nesting_box")
+            .order_by("-laid_at")[:10],
+        "sensors": HardwareSensor.objects.all().order_by("name"),
+        "latest_image": NestingBoxImage.objects.order_by("-created_at").first(),
+    }
+
+
 class DashboardView(TemplateView):
     template_name = "web_app/dashboard.html"
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-
-        today_start = timezone.localtime().replace(
-            hour=0, minute=0, second=0, microsecond=0
-        )
-        now = timezone.localtime()
-
-        eggs_today_qs = Egg.objects.filter(laid_at__range=(today_start, now))
-        ctx["eggs_today"] = eggs_today_qs.count()
-        ctx["laid_chickens"] = (
-            Chicken.objects.filter(egg__in=eggs_today_qs).distinct().order_by("name")
-        )
-
-        if db.connection.vendor == "postgresql":
-            latest_presence = (
-                NestingBoxPresence.objects.filter(present_at__gte=today_start)
-                .order_by("nesting_box_id", "-present_at")
-                .distinct("nesting_box_id")  # Sqlite doesn't support this
-            )
-        else:
-            latest_presence = []
-
-        ctx["latest_presence"] = latest_presence
-
-        ctx["latest_events"] = (
-            Egg.objects.filter(laid_at__gte=today_start)
-            .select_related("chicken", "nesting_box")
-            .order_by("-laid_at")[:10]
-        )
-        ctx["sensors"] = HardwareSensor.objects.all().order_by("name")
-        ctx["latest_image"] = NestingBoxImage.objects.order_by("-created_at").first()
+        ctx.update(get_dashboard_context())
         return ctx
+
+
+def partial_eggs_today(request):
+    return render(request, "web_app/partials/_eggs_today.html", get_dashboard_context())
+
+
+def partial_laid_chickens(request):
+    return render(request, "web_app/partials/_laid_chickens.html", get_dashboard_context())
+
+
+def partial_sensors(request):
+    return render(request, "web_app/partials/_sensors.html", get_dashboard_context())
+
+
+def partial_latest_image(request):
+    return render(request, "web_app/partials/_latest_image.html", get_dashboard_context())
+
+
+def partial_latest_presence(request):
+    return render(request, "web_app/partials/_latest_presence.html", get_dashboard_context())
+
+
+def partial_latest_events(request):
+    return render(request, "web_app/partials/_latest_events.html", get_dashboard_context())
 
 
 class ChickenListView(ListView):
